a06f8ef26b514e0713a841cb4370b9eebd82e4e2
Khusan <husan.nishonov99@gmail.com>
2024-05-29 20:43:03 +0500

    changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -92,6 +92,8 @@ export const BentoGridItem = ({
       {/* add img divs */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
+
+
           {img && (
             <img
               src={img}
@@ -99,6 +101,8 @@ export const BentoGridItem = ({
               className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
+
+
         </div>
         <div
           className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
@@ -108,10 +112,12 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              //   width={220}
+                // width={220}
               className="object-cover object-center w-full h-full"
             />
           )}
+
+
         </div>
         {id === 6 && (
           // add background animation , remove the p tag
